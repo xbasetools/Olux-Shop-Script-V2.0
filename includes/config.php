1873ad536c8c418d201bcd29fb6fe0b85c8ee670
xbaseleet <127048636+Skybadests@users.noreply.github.com>
2023-03-06 01:05:17 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,7 @@
 <?php
 
 
-$dbcon = mysqli_connect("localhost","artmir","Omeri1233","artmir") or die('enable connection'); 
-
+$dbcon = mysqli_connect("localhost","artmir","xbase_leet","") or die( connection'); 
 
 
 ?>
